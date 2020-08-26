cbc28c179a74646a187331aa767bacebb232d8c0
martincasti <martin.castineira@davinci.edu.ar>
2020-08-25 23:12:10 -0300

    Agrego ejercicio 1 a la carpeta 25082020

---
+++ 25082020/Ejercicio1/Ejercicio1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Ejercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nro = 10;
+            string nroGuardado = "";
+
+            Console.Write("Ingrese un numero: ");
+            nroGuardado = Console.ReadLine();
+            int valor = int.Parse(nroGuardado);
+            while (valor != 10)
+            {
+                Console.Write("Segui participando! Ingrese un numero: ");
+                nroGuardado = Console.ReadLine();
+                valor = int.Parse(nroGuardado);
+
+            }
+
+            Console.Write("Felicitaciones, Ganaste! ");
+            Console.ReadKey();
+
+            }
+        }
+    }
